0f6dd1c105ba7f0955af850e72aeb913d9b8c9ad
Dominik_Gadzina <epros98@gmail.com>
2022-07-23 23:35:45 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TvComponent } from './sections/tv/tv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TvComponent
   ],
   imports: [
     BrowserModule,
