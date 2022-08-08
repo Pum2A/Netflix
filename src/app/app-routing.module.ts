e1182e88c80c8a7dfa45e2ebcf9197d998a44567
Dominik_Gadzina <epros98@gmail.com>
2022-08-09 00:54:31 +0200

    login help added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './sections/login/login.component';
-
+import { MainComponent } from './sections/main/main.component';
+import { LoginHelpComponent } from './sections/login-help/login-help.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'LoginHelp', component: LoginHelpComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './sections/login/login.component';
 import { MainComponent } from './sections/main/main.component';
+import { LoginHelpComponent } from './sections/login-help/login-help.component';
 
 const appRoute: Routes = [
   {path: 'Login', component: LoginComponent },
-  {path: 'main', component: MainComponent}
+  {path: 'main', component: MainComponent},
+  {path: 'LoginHelp', component: LoginHelpComponent},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
     MainComponent,
+    LoginComponent,
+    LoginHelpComponent,
   ],
   imports: [
     BrowserModule,
