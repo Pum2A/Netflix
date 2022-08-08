f45b543e38bb1a3aa77cfa0c8fb470a60a74504c
Dominik_Gadzina <epros98@gmail.com>
2022-08-08 20:05:25 +0200

    section & login added, login box added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './sections/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TvComponent } from './sections/tv/tv.component';
+import { LoginComponent } from './sections/login/login.component';
+import { MainComponent } from './sections/main/main.component';
+
+const appRoute: Routes = [
+  {path: 'Login', component: LoginComponent },
+  {path: 'main', component: MainComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    TvComponent
+    routingComponents,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
